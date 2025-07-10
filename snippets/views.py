889430c32ac1bdca64396f5831667df5ee382e59
MustafaHaider523 <mustafahaider523@gmail.com>
2025-07-10 15:24:05 +0500

    add permission to SnipperList and SnipperDetails

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -12,10 +13,14 @@ class SnippetList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 
